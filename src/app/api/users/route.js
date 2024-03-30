3e59ed6121e303af08a9144a244e45d1071e8b35
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-03-30 20:32:14 -0300

    feat: create route get user by email

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -5,5 +5,5 @@ const service = new UserService();
 export async function POST(req) {
   const body = await req.json();
   const resp = await service.createUser(body);
-  return new Response(resp);
+  return Response.json(resp);
 }
