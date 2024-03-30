486ca0863d905ffc794149a47b2195bc6c3ab63a
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-03-30 20:32:56 -0300

    feat: create authenticator

---
+++ src/app/api/auth/auth.js
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./[...nextauth]";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import * as z from "zod";
+import { getUser } from "@/app/login/lib/getUserr";
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          console.log({ passwordsMatch });
+
+          if (passwordsMatch) return user;
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+});
+
+// export function auth(...args) {
+//   return getServerSession(...args, config);
+// }
---
+++ src/utils/validateExp.js
@@ -0,0 +1,1 @@
+export const validateExp = (exp) => exp * 1000 >= new Date().getTime();
