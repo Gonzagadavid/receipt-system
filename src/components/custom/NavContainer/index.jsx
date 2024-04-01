0df6ddbadc117ff84558617a3b5a490ca447dfba
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-04-01 00:08:59 -0300

    feat: add session provider

---
+++ src/components/custom/NavContainer/index.jsx
@@ -0,0 +1,32 @@
+"use client";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { routesInfo } from "@/constants/routes";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { useMemo } from "react";
+export default function NavContainer() {
+  const { data } = useSession();
+  const routes = useMemo(
+    () =>
+      Object.keys(routesInfo).map((route) =>
+        routesInfo[route].role.includes(data?.role) ? (
+          <Link key={route} href={route}>
+            <Card className="bg-secondary px-8 py-4 m-6 w-96 text-primary">
+              <CardContent className="flex justify-center">
+                {routesInfo[route].icon}
+              </CardContent>
+              <CardFooter className="flex justify-center">
+                <p className="text-2xl align-center">
+                  {routesInfo[route].label}
+                </p>
+              </CardFooter>
+            </Card>
+          </Link>
+        ) : null
+      ),
+    [data?.role]
+  );
+  return (
+    <div className="flex w-[90%] flex-wrap justify-self-center">{routes}</div>
+  );
+}
--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,7 @@
 import { getToken } from "next-auth/jwt";
 import { validateExp } from "./utils/validateExp";
 import { Routes } from "./constants/routes";
+import { NextResponse } from "next/server";
 
 export async function middleware(request) {
   const token = await getToken({
@@ -8,9 +9,9 @@ export async function middleware(request) {
     secret: process.env.AUTH_SECRET,
   });
   if (!token || (token && !validateExp(token.exp)))
-    request.redirect(new URL(Routes.login, request.url));
+    return NextResponse.redirect(new URL(Routes.login, request.url));
 }
 
 export const config = {
-  matcher: ["/((?!api|login|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/((?!api|login|users|_next/static|_next/image|.*\\.png$).*)"],
 };
