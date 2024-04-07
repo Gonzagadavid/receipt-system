8526bb191a88aac81843c7c69a54ceab5f28385e
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-04-07 18:02:54 -0300

    fix: adjust user exists verification

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,9 +1,18 @@
+import { httpStatusCode } from "../httpStatusCode";
 import UserService from "./service";
 
 const service = new UserService();
 
 export async function POST(req) {
   const body = await req.json();
+
+  const exists = await service.getUserByEmail(body.email);
+  console.log(exists);
+  if (exists)
+    return Response.json(
+      { message: "User Already registered" },
+      { status: httpStatusCode.Conflict }
+    );
 
   const resp = await service.createUser(body);
   return Response.json(resp);
