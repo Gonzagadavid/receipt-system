808ba11b8a0c6a5a422e6064b869ba34c62ab49f
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-04-07 18:05:30 -0300

    feat: create product register

---
+++ src/app/products/utils/index.js
@@ -0,0 +1,20 @@
+export const toLabelCategory = {
+  fish: "peixe",
+  food: "alimento",
+  condiment: "condimento",
+  beverage: "bebida",
+};
+
+export const decodeCategory = {
+  peixe: "fish",
+  alimento: "food",
+  condimento: "condiment",
+  bebida: "beverage",
+};
+
+export const toLabelState = {
+  frozen: "congelado",
+  fresh: "fresco",
+  industrialized: "industrializado",
+  handmade: "artesanal",
+};
