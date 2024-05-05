d5bd82e32e0cf44f99b2ceb1dc895fd166a850b0
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-05-05 17:54:49 -0300

    fix: adjust error message

--- src/app/login/lib/getUser.js
+++ src/app/login/lib/getUser.js
@@ -3,7 +3,7 @@ export const getUser = async (email) => {
   const resp = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${email}`,
   );
-  if (!resp.ok) throw new Error("User Error");
+  if (!resp.ok) throw new Error("Get user information error");
   const user = await resp.json().catch(() => null);
   return user;
 };
