827319885839ad109c36e2daa10487fe83e8c7a8
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-05-05 17:14:35 -0300

    fix: remove icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,6 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/basket.svg" type="image/svg+xml"></link>
       <body className={inter.className}>
         <SessionProvider
           refetchOnWindowFocus={false}
