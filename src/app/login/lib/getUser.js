0070b1b18807c270ff2a1da4c12fb6cfd7382da4
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-05-05 17:45:12 -0300

    fix: adjust env name

--- src/app/login/lib/getUser.js
+++ src/app/login/lib/getUser.js
@@ -1,7 +1,7 @@
 "use server";
 export const getUser = async (email) => {
   const resp = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_UR}/api/users/${email}`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${email}`,
   );
   if (!resp.ok) throw new Error("User Error");
   const user = await resp.json().catch(() => null);
