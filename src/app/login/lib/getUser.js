bb05dcd4095a22008510e4a9dd2cd9be3648bcd4
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-05-05 17:35:02 -0300

    fix: adjust getUser url

--- src/app/login/lib/getUser.js
+++ src/app/login/lib/getUser.js
@@ -1,6 +1,8 @@
 "use server";
 export const getUser = async (email) => {
-  const resp = await fetch("http://localhost:3000/api/users/" + email);
+  const resp = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_UR}/api/users/${email}`,
+  );
   if (!resp.ok) throw new Error("User Error");
   const user = await resp.json().catch(() => null);
   return user;
